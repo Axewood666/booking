ef4cc6857781d692953a3dcfc0f06c9592dfafaf
Igor Zhugar <igor.zhugar@mail.ru>
2025-11-08 22:45:37 +0300

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -8,10 +8,28 @@ CREATE TABLE IF NOT EXISTS events (
 
 CREATE TABLE IF NOT EXISTS bookings (
   id SERIAL PRIMARY KEY,
-  event_id INT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+  event_id INT NOT NULL REFERENCES events(id),
   user_id VARCHAR NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   UNIQUE (event_id, user_id)
 );
+
+INSERT INTO events (
+	name,
+	total_seats
+) VALUES ('first_test', 2);
+
+INSERT INTO events (
+	name,
+	total_seats
+) VALUES ('second_test', 1);
+
+INSERT INTO events (
+	name,
+	total_seats
+) VALUES ('third_test', 2);
+
+INSERT INTO bookings(event_id, user_id)
+VALUES (3, 'user321')
 
 COMMIT;
